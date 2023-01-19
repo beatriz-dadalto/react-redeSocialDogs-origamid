9d4b48f6d5f89f31f1cc31080a9fa02fde18c679
Beatriz Dadalto <biacoelho@proton.me>
2023-01-19 18:12:13 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
 function App() {
   return (
-    <div>App React</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Footer />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
