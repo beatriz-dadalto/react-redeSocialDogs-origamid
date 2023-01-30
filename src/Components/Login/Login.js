5fb86667f68eac2e311b3e56e8495cd0f0452757
Beatriz Dadalto <biacoelho@proton.me>
2023-01-30 18:20:05 -0300

    Login Background

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,23 +5,26 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) {
-    return <Navigate to="/conta"/>
+    return <Navigate to="/conta" />;
   }
-  
+
   return (
-    <div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="criar" element={<LoginCreate />} />
         <Route path="perdeu" element={<LoginPasswordLost />} />
         <Route path="criar" element={<LoginPasswordReset />} />
       </Routes>
-    </div>
+      </div>
+    </section>
   );
 };
 
