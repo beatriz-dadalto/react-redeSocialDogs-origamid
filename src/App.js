fca87b908e1a93fd113a5c309ea35daaab146843
Beatriz Dadalto <biacoelho@proton.me>
2023-04-20 17:13:15 -0300

    Perfil Usuário

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from './Components/Login/Login';
 import Photo from './Components/Photo/Photo';
 import User from './Components/User/User';
 import UserStorage from './UserContext';
+import UserProfile from './Components/User/UserProfile';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
               }
             />
             <Route path='foto/:id' element={<Photo />}/>
+            <Route path='perfil/:user' element={<UserProfile />}/>
           </Routes>
           <Footer />
         </UserStorage>
