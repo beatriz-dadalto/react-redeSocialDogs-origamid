6b1324567fe2d73a4a6481e39cd401723d7fb2ca
Beatriz Dadalto <biacoelho@proton.me>
2023-05-13 18:47:51 -0300

    Ajustes Finais

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -34,7 +34,7 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-        <Error error={error} />
+        <Error error={error && 'Dados incorretos.'} />
       </form>
       <Link to="/login/perdeu" className={styles.perdeu}>
         Perdeu a senha?
